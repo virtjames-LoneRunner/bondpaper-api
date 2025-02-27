dd4196501696e357648154a3f3c025457b6f7739
James Virtudazo <virtjameslourd55@gmail.com>
2025-02-27 21:18:34 +0800

    ...

--- include/config.py
+++ include/config.py
@@ -1,13 +1,13 @@
 # Setup Variables
 a4_step_motors = {
     'stepper_one': {
-        'dir_pin': 0,
-        'pulse_pin': 0,
-        'steps': 0
+        'dir_pin': 27,
+        'pulse_pin': 17,
+        'steps': 1000
     },
     'stepper_two': {
-        'dir_pin': 0,
-        'pulse_pin': 0,
+        'dir_pin': 5,
+        'pulse_pin': 6,
         'steps': 0
     }
 }
@@ -23,3 +23,4 @@ long_step_motors = {
         'steps': 0
     }
 }
+
